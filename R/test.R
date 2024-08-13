845549091d990a90e4c31ddd81bb9ec67bb7ca91
Lovemercedes <166413158+DSMercedes@users.noreply.github.com>
2024-08-13 19:42:59 +0800

    Create test.R

---
+++ R/test.R
@@ -0,0 +1,12 @@
+
+
+library(NMGS)
+dyn.load("/usr/lib/libgsl.so", local = FALSE, now = FALSE)
+dyn.load("/usr/lib/libgslcblas.so", local = FALSE, now = FALSE)
+dyn.load("pkg/src/NMGS.so")
+
+
+#testf(2)
+#.Call("safeexp", as.double(10), PACKAGE = "NMGS")
+#.Call("Sum", as.integer(1), as.integer(2), PACKAGE = "NMGS")
+#.C("Sum")
