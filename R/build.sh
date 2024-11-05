7a9ffb4a28d0ccafe8ba833112c604722ca155c7
Lovemercedes <166413158+DSMercedes@users.noreply.github.com>
2024-11-05 16:03:33 +0800

    Create build.sh

---
+++ R/build.sh
@@ -0,0 +1,8 @@
+/usr/bin/R CMD BATCH document.R 
+/usr/bin/R CMD check pkg
+/usr/bin/R CMD build pkg
+/usr/bin/R CMD INSTALL NMGS_0.1.03.tar.gz
+<<<<<<< HEAD
+=======
+
+>>>>>>> ae186b09b348f3f8c693c9677b0dcfcdc700095c
